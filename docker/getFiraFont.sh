5107b2383eb3fe238c51c5f32fec2cb2530ff987
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2016-04-20 09:02:31 +0200

    Fix #206: update Fira Sans font URLs

--- docker/getFiraFont.sh
+++ docker/getFiraFont.sh
@@ -6,11 +6,11 @@ cd /tmp
 
 # install unzip just in case the user doesn't already have it.
 sudo apt-get install unzip -y
-wget "http://dev.carrois.com/wordpress/wp-content/uploads/downloads/fira_3_1/FiraSans3106.zip"
-wget "http://dev.carrois.com/wordpress/wp-content/uploads/downloads/fira_3_1/FiraMono3106.zip"
+wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
+wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"
 
-unzip FiraMono3106.zip
-unzip FiraSans3106.zip
+unzip FiraSans4106.zip
+unzip FiraMono3206.zip
 
 sudo mkdir -p /usr/share/fonts/truetype/FiraSans
 sudo mkdir -p /usr/share/fonts/opentype/FiraSans
