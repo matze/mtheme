539f6f4adb55618867cc51cdb129ab1ec12b2d8c
arijitlaik <arijit.netcom@gmail.com>
2016-08-11 04:44:11 +0530

    update unzip filenames
    the unzip command used filenames from the previous urls the new urls have different file names, this fixes the script

--- docker/getFiraFont.sh
+++ docker/getFiraFont.sh
@@ -9,8 +9,8 @@ sudo apt-get install unzip -y
 wget "http://www.carrois.com/downloads/fira_4_1/FiraFonts4106.zip"
 wget "http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip"
 
-unzip FiraSans4106.zip
-unzip FiraMono3206.zip
+unzip FiraFonts4106.zip
+unzip FiraMonoFonts3206.zip
 
 sudo mkdir -p /usr/share/fonts/truetype/FiraSans
 sudo mkdir -p /usr/share/fonts/opentype/FiraSans
